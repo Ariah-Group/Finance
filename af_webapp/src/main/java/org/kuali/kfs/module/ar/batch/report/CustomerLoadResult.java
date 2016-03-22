b197a0a59b9e4d29078feba85186b44860bf9d52
Ariah Group <code@ariahgroup.org>
2016-03-22 13:47:51 -0400

    fix private method that should be public

--- af_webapp/src/main/java/org/kuali/kfs/module/ar/batch/report/CustomerLoadResult.java
+++ af_webapp/src/main/java/org/kuali/kfs/module/ar/batch/report/CustomerLoadResult.java
@@ -90,7 +90,7 @@ public class CustomerLoadResult {
         return getResultCodeString(result);
     }
     
-    private void setResult(ResultCode result) {
+    public void setResult(ResultCode result) {
         this.result = result;
     }
     
